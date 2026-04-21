be87ae8beb2922d1ca62e1ec570de8a0caa2bf35
karpix25 <nadaraya@gmail.com>
2026-04-20 21:42:13 -0300

    fix: mark AxiosInstance as type import to resolve tsx ESM SyntaxError

--- src/services/instagram.service.ts
+++ src/services/instagram.service.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { type AxiosInstance } from 'axios';
 import fs from 'fs-extra';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
